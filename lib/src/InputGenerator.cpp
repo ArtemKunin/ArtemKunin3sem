0b8b1c6b90d48deb0d840129cf127ca6566aac37
Артем Кунин ТКИ-241 <artem.kunin67@gmail.com>
2026-09-21 16:50:51 +0300

    Update InputGenerator.cpp

--- lib/src/InputGenerator.cpp
+++ lib/src/InputGenerator.cpp
@@ -1,1 +1,11 @@
+#include "InputGenerator.h"
+#include <iostream>
 
+InputGenerator::InputGenerator(std::istream &input) : input_stream(input) {}
+
+int InputGenerator::generate()
+{
+    int value = 0;
+    this->input_stream >> value;
+    return value;
+}
