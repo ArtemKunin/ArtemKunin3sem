91aec5ce784fabda715e0ee4823678755eaac2bb
Артем Кунин ТКИ-241 <artem.kunin67@gmail.com>
2026-09-21 16:46:59 +0300

    Update Matrix.h

--- lib/include/Matrix.h
+++ lib/include/Matrix.h
@@ -1,1 +1,145 @@
+#pragma once
+#include <iosfwd>
+#include <string>
+#include "Generator.h"
 
+/**
+ * @brief Класс двумерного массива
+ */
+class Matrix
+{
+private:
+    /**
+     * @brief Количество строк
+     */
+    size_t row_count;
+
+    /**
+     * @brief Количество столбцов
+     */
+    size_t col_count;
+
+    /**
+     * @brief Указатель на массив
+     */
+    int **matrix_data;
+
+public:
+    /**
+     * @brief Конструктор по умолчанию
+     */
+    Matrix();
+
+    /**
+     * @brief Конструктор матрицы rows * columns, заполненой нулями
+     * @param rows Количество строк
+     * @param cols Количество столбцов
+     */
+    Matrix(const size_t rows, const size_t cols);
+
+    /**
+     * @brief Конструктор копирования
+     * @param other Ссылка на другую матрицу
+     */
+    Matrix(const Matrix &other);
+
+    /**
+     * @brief Конструктор перемещения
+     * @param other Перемещаемый объект
+     */
+    Matrix(Matrix &&other) noexcept;
+
+    /**
+     * @brief Деструктор
+     */
+    ~Matrix();
+
+    /**
+     * @brief Оператор присваивания копированием
+     * @param other Ссылка на другую матрицу
+     * @return Ссылка на скопированную матрицу
+     */
+    Matrix &operator=(const Matrix &other);
+
+    /**
+     * @brief Оператор присваивания перемещением
+     * @param other Перемещаемый объект
+     * @return Ссылка на перемещенную матрицу
+     */
+    Matrix &operator=(Matrix &&other) noexcept;
+
+    /**
+     * @brief Перегрузка оператора вывода
+     * @param os Поток вывода
+     * @param matrix Матрица для вывода
+     * @return Перегруженный поток вывода
+     */
+    friend std::ostream &operator<<(std::ostream &os, const Matrix &matrix);
+
+    /**
+     * @brief Перегрузка оператора ввода
+     * @param is Поток ввода
+     * @param matrix Матрица для ввода
+     * @return Перегруженный поток ввода
+     */
+    friend std::istream &operator>>(std::istream &is, Matrix &matrix);
+
+    /**
+     * @brief Оператор проверки на равенство
+     * @param other - ссылка на другую матрицу
+     * @return 1 - если матрицы равны, иначе 0
+     */
+    bool operator==(const Matrix &other) const;
+
+    /**
+     * @brief Оператор проверки на неравенство
+     * @param other - ссылка на другую матрицу
+     * @return 1 - если матрицы не равны, иначе 0
+     */
+    bool operator!=(const Matrix &other) const;
+
+    /**
+     * @brief Преобразование матрицы в строку
+     * @return Содержимое строки
+     */
+    std::string toString() const;
+
+    /**
+     * @brief Доступ к строке матрицы (только чтение)
+     * @param index - номер элемента
+     * @return Элемент по индексу
+     */
+    const int *operator[](const size_t index) const;
+
+    /**
+     * @brief Доступ к строке матрицы (чтение и запись)
+     * @param index - номер элемента
+     * @return элемент по индексу
+     */
+    int *operator[](const size_t index);
+
+    /**
+     * @brief Получить количество строк
+     * @return Количество строк
+     */
+    size_t getRows() const noexcept;
+
+    /**
+     * @brief Получить количество столбцов
+     * @return Количество столбцов
+     */
+    size_t getColumns() const noexcept;
+
+    /**
+     * @brief Вставка новой строки после заданного индекса
+     * @param index Индекс, после которого вставляется строка
+     * @param new_row Указатель на массив значений новой строки
+     */
+    void insertRowAfter(const size_t index, const int *new_row);
+
+    /**
+     * @brief Заполнение матрицы с помощью абстрактного класса Generator
+     * @param generator - результат выполнения функции Generator
+     */
+    void fillArray(Generator &generator);
+};
