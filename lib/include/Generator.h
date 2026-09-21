717d5317635d770041cbfc9aae85521b48f7c2c2
Артем Кунин ТКИ-241 <artem.kunin67@gmail.com>
2026-09-21 16:46:00 +0300

    Update Generator.h

--- lib/include/Generator.h
+++ lib/include/Generator.h
@@ -1,1 +1,19 @@
+#pragma once
 
+/**
+ * @brief Абстрактный класс Generator
+ */
+class Generator
+{
+public:
+    /**
+     * @brief Виртуальный деструктор по умолчанию
+     */
+    virtual ~Generator() = default;
+
+    /**
+     * @brief Виртуальный "генератор" значений массива
+     * @return Сгенерированное целочисленное значение
+     */
+    virtual int generate() = 0;
+};
