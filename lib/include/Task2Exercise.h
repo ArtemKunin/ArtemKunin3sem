4901ae2d7cac2bbd9d7c24a62ee8bbae5edf298a
Артем Кунин ТКИ-241 <artem.kunin67@gmail.com>
2026-09-21 16:49:01 +0300

    Update Task2Exercise.h

--- lib/include/Task2Exercise.h
+++ lib/include/Task2Exercise.h
@@ -1,1 +1,28 @@
+#pragma once
 
+#include "Exercise.h"
+
+/**
+ * @brief класс Task2Exercise абстрактного класса Exercise
+ */
+class Task2Exercise : public Exercise
+{
+public:
+    /**
+     * @brief Конструктор
+     * @param src Ссылка на матрицу
+     * @param gen Ссылка на генератор
+     */
+    Task2Exercise(Matrix &src, Generator &gen);
+
+    /**
+     * @brief Перегруженный деструктор
+     */
+    ~Task2Exercise() override = default;
+
+    /**
+     * @brief Выполнить задание 2 (вставить строку 1,2,3... после строк с минимумом)
+     * @return Выполненное задание (новая матрица)
+     */
+    Matrix solve() override;
+};
