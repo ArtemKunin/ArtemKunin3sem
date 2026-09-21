a7fd3c84095b9152a02d0a93af9e54b3230e4aa1
Артем Кунин ТКИ-241 <artem.kunin67@gmail.com>
2026-09-21 16:52:08 +0300

    Update Task1Exercise.cpp

--- lib/src/Task1Exercise.cpp
+++ lib/src/Task1Exercise.cpp
@@ -1,1 +1,27 @@
+#include "Task1Exercise.h"
 
+Task1Exercise::Task1Exercise(Matrix &src, Generator &gen) : Exercise(src, gen) {}
+
+Matrix Task1Exercise::solve()
+{
+    Matrix result(source);
+
+    const size_t rows = result.getRows();
+    const size_t columns = result.getColumns();
+    if (rows == 0 || columns == 0)
+    {
+        return result;
+    }
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < columns; j++)
+        {
+            if (result[i][j] % 2 == 0)
+            {
+                result[i][j] = 0;
+            }
+        }
+    }
+
+    return result;
+}
