0666af2cd729f98816eb8014c34931ea73c75eb7
Артем Кунин ТКИ-241 <artem.kunin67@gmail.com>
2026-09-21 16:45:39 +0300

    Update Exercise.h

--- lib/include/Exercise.h
+++ lib/include/Exercise.h
@@ -1,1 +1,40 @@
+#pragma once
 
+#include "Matrix.h"
+#include "Generator.h"
+
+/**
+ * @brief Виртуальный класс exercise
+ */
+class Exercise
+{
+protected:
+    /**
+     * @brief Ссылка на исходную матрицу
+     */
+    Matrix &source;
+
+    /**
+     * @brief Ссылка на генератор значений
+     */
+    Generator &generator;
+
+public:
+    /**
+     * @brief Конструктор
+     * @param src Ссылка на исходную матрицу
+     * @param gen Ссылка на генератор
+     */
+    Exercise(Matrix &src, Generator &gen);
+
+    /**
+     * @brief Виртуальный деструктор
+     */
+    virtual ~Exercise() = default;
+
+    /**
+     * @brief Виртуальный метод решения задания
+     * @return Новая матрица - результат решения задания
+     */
+    virtual Matrix solve() = 0;
+};
