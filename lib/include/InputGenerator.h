b1b1b3ec6a5a49377f4f5b474ad280787321269d
Артем Кунин ТКИ-241 <artem.kunin67@gmail.com>
2026-09-21 16:46:36 +0300

    Update InputGenerator.h

--- lib/include/InputGenerator.h
+++ lib/include/InputGenerator.h
@@ -1,1 +1,33 @@
+#pragma once
+#include <iosfwd>
+#include "Generator.h"
 
+/**
+ * @brief Класс InputGenerator абстрактного класса Generator
+ */
+class InputGenerator : public Generator
+{
+private:
+    /**
+     * @brief Ссылка на поток ввода
+     */
+    std::istream &input_stream;
+
+public:
+    /**
+     * @brief Конструктор
+     * @param input Ссылка на используемый поток ввода
+     */
+    explicit InputGenerator(std::istream &input);
+
+    /**
+     * @brief Деструктор
+     */
+    ~InputGenerator() override = default;
+
+    /**
+     * @brief Считывание числа из потока
+     * @return Считанное число, иначе 0 в случае ошибки ввода
+     */
+    int generate() override;
+};
