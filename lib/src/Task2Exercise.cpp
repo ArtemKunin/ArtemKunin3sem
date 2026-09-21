55bf3799674a3b6fd42b6deab42e850963b1dea3
Артем Кунин ТКИ-241 <artem.kunin67@gmail.com>
2026-09-21 16:52:28 +0300

    Update Task2Exercise.cpp

--- lib/src/Task2Exercise.cpp
+++ lib/src/Task2Exercise.cpp
@@ -1,1 +1,53 @@
+#include "Task2Exercise.h"
 
+Task2Exercise::Task2Exercise(Matrix &src, Generator &gen) : Exercise(src, gen) {}
+
+Matrix Task2Exercise::solve()
+{
+    const size_t rows = source.getRows();
+    const size_t columns = source.getColumns();
+
+    if (rows == 0 || columns == 0)
+    {
+        return Matrix();
+    }
+    int minValue = source[0][0];
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < columns; j++)
+        {
+            if (source[i][j] < minValue)
+            {
+                minValue = source[i][j];
+            }
+        }
+    }
+    int *sequenceRow = new int[columns];
+    for (size_t j = 0; j < columns; j++)
+    {
+        sequenceRow[j] = j + 1;
+    }
+    Matrix result(source);
+    for (size_t i = result.getRows(); i > 0; --i)
+    {
+        size_t actual_index = i - 1;
+        bool hasMin = false;
+
+        for (size_t j = 0; j < columns; j++)
+        {
+            if (result[actual_index][j] == minValue)
+            {
+                hasMin = true;
+                break;
+            }
+        }
+
+        if (hasMin)
+        {
+            result.insertRowAfter(actual_index, sequenceRow);
+        }
+    }
+    delete[] sequenceRow;
+
+    return result;
+}
